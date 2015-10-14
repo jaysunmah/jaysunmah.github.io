bc27f6fee7e42f7a18e880d4596a3136243046b0
jaysunmah <jaysunmah@gmail.com>
2015-10-14 01:07:30 -0400

    isishard

--- EMS/screenSaver/sketch.js
+++ EMS/screenSaver/sketch.js
@@ -7,29 +7,29 @@ var lines = []
 function setup() 
 {
   background(0);
-  createCanvas(500, 500); 
+  createCanvas(windowWidth, windowHeight); 
   noStroke();  
   bug = new Jitter(1);
 }
 function draw() 
 { 
   
- background(0, 0, 0, 1);
+ background(100, 100, 255, 1);
   i = random(10);
-  x1 = random(600);
-  x2 = random(600);
+  x1 = random(windowWidth);
+  x2 = random(windowWidth);
   sumX = abs(x2 - x1);
-  y1 = random(600);
-  y2 = random(600);
+  y1 = random(windowHeight);
+  y2 = random(windowHeight);
   sumY = abs(y2 - y1);
-  if (i > 4) {
+  if (i > 2) {
     lines.push(new Jitter(i % 2, sumX, sumY))
     print(i % 2)
   }
   for (var i=0; i<lines.length; i++) {
     lines[i].move();
     lines[i].display();
-    if (lines[i].x > 700 || lines[i].y > 700) {
+    if (lines[i].x > windowWidth + 100 || lines[i].y > windowHeight + 100) {
       lines.splice(i, 1);
     }
   }
