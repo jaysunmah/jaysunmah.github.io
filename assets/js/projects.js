46cc072bd5c84a70b0aa8b85d781f909936c476a
Jason Ma <jaysunmah@gmail.com>
2017-04-29 00:50:45 -0400

    added filtered links

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -14,17 +14,17 @@ $(document).ready(function() {
   var imHeight = $('#refIm').height();
   // $('.ui.portfolio.grid .column').css('height', imHeight);
   $('.ui.portfolio.grid .column .ui.fade.reveal').css('height', imHeight);
-  $('.ui.menu')
-    .on('click', '.item', function() {
-      var item = (this.id);
-      menuHandler(item);
-      if(!$(this).hasClass('dropdown')) {
-        $(this)
-          .addClass('active')
-          .siblings('.item')
-          .removeClass('active');
-      }
-    });
+  // $('.ui.menu')
+  //   .on('click', '.item', function() {
+  //     var item = (this.id);
+  //     menuHandler(item);
+  //     if(!$(this).hasClass('dropdown')) {
+  //       $(this)
+  //         .addClass('active')
+  //         .siblings('.item')
+  //         .removeClass('active');
+  //     }
+  //   });
 });
 
 $( window ).resize(function() {
