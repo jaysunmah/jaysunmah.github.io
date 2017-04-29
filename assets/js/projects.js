140faf0dc1cf9f0701ae6a2775baa2a69f9cd336
Jason Ma <jaysunmah@gmail.com>
2017-04-29 14:01:18 -0400

    added templating

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -10,10 +10,71 @@ function menuHandler(item) {
   }
 }
 
+function renderProjects() {
+  var source   = $("#projectsContainer").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    projects: [
+      {
+        title: "Sensel Classifier",
+        imSource: "../assets/images/project_icons/sensel800x400.png",
+        date: "April 2017 - May 2017",
+        description: "Hand and foot classifier based on inputs from a pressure module"
+      },
+      {
+        title: "Weija Messenger Bot",
+        imSource: "../assets/images/project_icons/weija800x400.png" ,
+        date: "March 2017 - May 2017",
+        description: "Facebook messenger bot with multiple features such as bus tracking and dad jokes"
+      },
+      {
+        title: "Raspi Car",
+        imSource: "../assets/images/project_icons/raspicar800x400.png",
+        date: "March 2017 - April 2017",
+        description: "Raspberry Pi controlled car. Future plans to implement self driving"
+      },
+      {
+        title: "Youtube Scraper",
+        imSource: "../assets/images/project_icons/youtubescraper800x400.png",
+        date: "Feburary 2017 - March 2017",
+        description: "Self portait machine to scrape youtube data and visualize it"
+      },
+      {
+        title: "Pittsburgh Bus API",
+        imSource: "../assets/images/project_icons/bustrakr800x400.png" ,
+        date: "January 2017 - Feburary 2017",
+        description: "Robust python addon to the Pittsburgh Port Authority bus API"
+      },
+      {
+        title: "Trapped Unity Game",
+        imSource: "../assets/images/project_icons/room800x400.png",
+        date: "November 2016 - December 2016<",
+        description: "Interactive multiplayer unity game where 2 players work together to escape a room"
+      },
+      {
+        title: "Rude Goldberg",
+        imSource: "../assets/images/project_icons/rubegoldberg.png",
+        date: "October 2016 - November 2016",
+        description: "Introductory Unity application with rudimentary unity physics"
+      },
+      {
+        title: "Bing Rewards Bot",
+        imSource: "../assets/images/project_icons/bingbots.png",
+        date: "August 2016",
+        description: "Automated scripts to log in and claim bing rewards"
+      },
+    ]
+  };
+  var html    = template(context);
+  $('#projectsRow').append(html)
+}
+
 $(document).ready(function() {
-  var imHeight = $('#refIm').height();
-  // $('.ui.portfolio.grid .column').css('height', imHeight);
-  $('.ui.portfolio.grid .column .ui.fade.reveal').css('height', imHeight);
+  renderProjects();
+  window.setTimeout(function () {
+    var imHeight = $('#refIm').height();
+    document.getElementById("refCol").style.height = imHeight + "px";
+  }, 5);
   // $('.ui.menu')
   //   .on('click', '.item', function() {
   //     var item = (this.id);
@@ -29,5 +90,5 @@ $(document).ready(function() {
 
 $( window ).resize(function() {
   var imHeight = $('#refIm').height();
-  $('.ui.portfolio.grid .column .ui.fade.reveal').css('height', imHeight);
+  document.getElementById("refCol").style.height = imHeight + "px";
 });
